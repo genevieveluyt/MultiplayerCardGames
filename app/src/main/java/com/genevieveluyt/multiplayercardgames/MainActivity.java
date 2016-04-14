01febbe312d9c923d4265a6859c4a9d4110f3259
Genevieve Luyt <genevieve.luyt@gmail.com>
2016-04-13 17:21:48 -0700

    Home page UI buttons update
    - buttons are different colours, stretch the width of the screen, and grey out when not signed in

--- app/src/main/java/com/genevieveluyt/multiplayercardgames/MainActivity.java
+++ app/src/main/java/com/genevieveluyt/multiplayercardgames/MainActivity.java
@@ -23,7 +23,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.widget.TextView;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -39,27 +39,6 @@ import com.google.android.gms.plus.Plus;
 
 import java.util.ArrayList;
 
-//import com.google.android.gms.games.Games;
-//import com.google.example.games.basegameutils.BaseGameUtils;
-
-
-/**
- * TBMPSkeleton: A minimalistic "game" that shows turn-based
- * multiplayer features for Play Games Services.  In this game, you
- * can invite a variable number of players and take turns editing a
- * shared state, which consists of single string.  You can also select
- * automatch players; all known players play before automatch slots
- * are filled.
- * <p/>
- * INSTRUCTIONS: To run this sample, please set up
- * a project in the Developer Console. Then, place your app ID on
- * res/values/ids.xml. Also, change the package name to the package name you
- * used to create the client ID in Developer Console. Make sure you sign the
- * APK with the certificate whose fingerprint you entered in Developer Console
- * when creating your Client Id.
- *
- * @author Wolff (wolff@google.com), 2013
- */
 public class MainActivity extends Activity
 		implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener,
 		View.OnClickListener {
@@ -146,6 +125,12 @@ public class MainActivity extends Activity
 	@Override
 	public void onConnected(Bundle connectionHint) {
 		Log.d(TAG, "onConnected(): Connection successful");
+		Button startMatchBtn = (Button) findViewById(R.id.startMatchButton);
+		Button checkMatchBtn = (Button) findViewById(R.id.checkGamesButton);
+		startMatchBtn.setClickable(true);
+		startMatchBtn.setBackgroundResource(R.color.new_match);
+		checkMatchBtn.setClickable(true);
+		checkMatchBtn.setBackgroundResource(R.color.check_match);
 
 		// Retrieve the TurnBasedMatch from the connectionHint
 		if (connectionHint != null) {
@@ -298,11 +283,17 @@ public class MainActivity extends Activity
 	// Update the visibility based on what state we're in.
 	public void setViewVisibility() {
 		boolean isSignedIn = (mGoogleApiClient != null) && (mGoogleApiClient.isConnected());
+		Button startMatchBtn = (Button) findViewById(R.id.startMatchButton);
+		Button checkMatchBtn = (Button) findViewById(R.id.checkGamesButton);
 
 		if (!isSignedIn) {
-			findViewById(R.id.login_layout).setVisibility(View.VISIBLE);
+			findViewById(R.id.menu_layout).setVisibility(View.VISIBLE);
+			findViewById(R.id.sign_out_button).setVisibility(View.GONE);
 			findViewById(R.id.sign_in_button).setVisibility(View.VISIBLE);
-			findViewById(R.id.matchup_layout).setVisibility(View.GONE);
+			startMatchBtn.setClickable(false);
+			startMatchBtn.setBackgroundResource(R.color.grey);
+			checkMatchBtn.setClickable(false);
+			checkMatchBtn.setBackgroundResource(R.color.grey);
 			findViewById(R.id.gameplay_layout).setVisibility(View.GONE);
 
 			if (mAlertDialog != null) {
@@ -311,16 +302,15 @@ public class MainActivity extends Activity
 			return;
 		}
 
-
-		((TextView) findViewById(R.id.name_field)).setText(Games.Players.getCurrentPlayer(
-				mGoogleApiClient).getDisplayName());
-		findViewById(R.id.login_layout).setVisibility(View.GONE);
-
 		if (isDoingTurn) {
-			findViewById(R.id.matchup_layout).setVisibility(View.GONE);
+			findViewById(R.id.menu_layout).setVisibility(View.GONE);
 			findViewById(R.id.gameplay_layout).setVisibility(View.VISIBLE);
 		} else {
-			findViewById(R.id.matchup_layout).setVisibility(View.VISIBLE);
+			findViewById(R.id.menu_layout).setVisibility(View.VISIBLE);
+			findViewById(R.id.sign_out_button).setVisibility(View.VISIBLE);
+			findViewById(R.id.sign_in_button).setVisibility(View.GONE);
+			findViewById(R.id.startMatchButton).setClickable(true);
+			findViewById(R.id.checkGamesButton).setClickable(true);
 			findViewById(R.id.gameplay_layout).setVisibility(View.GONE);
 		}
 	}
@@ -496,20 +486,6 @@ public class MainActivity extends Activity
 		String myParticipantId = mMatch.getParticipantId(playerId);
 
 		ArrayList<String> participantIds = mMatch.getParticipantIds();
-
-		/*int desiredIndex = -1;
-
-		for (int i = 0; i < participantIds.size(); i++) {
-			if (participantIds.get(i).equals(myParticipantId)) {
-				desiredIndex = i + 1;
-			}
-		}
-
-		if (desiredIndex < participantIds.size()) {
-			return participantIds.get(desiredIndex);
-		}
-
-		return participantIds.get(0); */
 
 		return participantIds.get((participantIds.indexOf(myParticipantId) + 1) % participantIds.size());
 	}
@@ -711,288 +687,3 @@ public class MainActivity extends Activity
 		}
 	}
 }
-
-/*
-import android.app.Activity;
-import android.content.DialogInterface;
-import android.content.DialogInterface.OnCancelListener;
-import android.content.Intent;
-import android.content.IntentSender.SendIntentException;
-import android.os.Bundle;
-import android.os.RemoteException;
-import android.service.carrier.CarrierMessagingService;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
-
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
-import com.google.android.gms.common.api.GoogleApiClient;
-
-import com.google.android.gms.common.api.ResultCallback;
-import com.google.android.gms.common.api.Status;
-import com.google.android.gms.games.Games;
-import com.google.android.gms.games.multiplayer.Multiplayer;
-import com.google.android.gms.games.multiplayer.realtime.RoomConfig;
-import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMatch;
-import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMatchConfig;
-import com.google.android.gms.games.multiplayer.turnbased.TurnBasedMultiplayer;
-import com.google.android.gms.plus.Plus;
-
-import java.util.ArrayList;
-
-public class MainActivity extends Activity implements
-		GoogleApiClient.ConnectionCallbacks,
-		GoogleApiClient.OnConnectionFailedListener {
-
-	private static final String TAG = "GooglePlayServicesActiv";
-
-	private static final String KEY_IN_RESOLUTION = "is_in_resolution";
-
-	// Request code for auto Google Play Services error resolution.
-	protected static final int REQUEST_CODE_RESOLUTION = 1;
-
-
-	// For our intents
-	final static int RC_SELECT_PLAYERS = 10000; // Copied from sample code, can this be any number??
-
-	// Google API client.
-	private GoogleApiClient mGoogleApiClient;
-
-	//Determines if the client is in a resolution state, and
-	//waiting for resolution intent to return.
-	private boolean mIsInResolution;
-
-	//Called when the activity is starting. Restores the activity state.
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		if (savedInstanceState != null) {
-			mIsInResolution = savedInstanceState.getBoolean(KEY_IN_RESOLUTION, false);
-		}
-	}
-
-	/**
-	 * Called when the Activity is made visible.
-	 * A connection to Play Services need to be initiated as
-	 * soon as the activity is visible. Registers {@code ConnectionCallbacks}
-	 * and {@code OnConnectionFailedListener} on the
-	 * activities itself.
-	 *//*
-	@Override
-	protected void onStart() {
-		super.onStart();
-		if (mGoogleApiClient == null) {
-			mGoogleApiClient = new GoogleApiClient.Builder(this)
-					.addApi(Games.API)
-					.addApi(Plus.API)
-					.addScope(Games.SCOPE_GAMES)
-					.addScope(Plus.SCOPE_PLUS_LOGIN)
-							// Optionally, add additional APIs and scopes if required.
-					.addConnectionCallbacks(this)
-					.addOnConnectionFailedListener(this)
-					.build();
-		}
-		mGoogleApiClient.connect();
-	}
-
-	/**
-	 * Called when activity gets invisible. Connection to Play Services needs to
-	 * be disconnected as soon as an activity is invisible.
-	 *//*
-	@Override
-	protected void onStop() {
-		if (mGoogleApiClient != null) {
-			mGoogleApiClient.disconnect();
-		}
-		super.onStop();
-	}
-
-	/**
-	 * Saves the resolution state.
-	 *//*
-	@Override
-	protected void onSaveInstanceState(Bundle outState) {
-		super.onSaveInstanceState(outState);
-		outState.putBoolean(KEY_IN_RESOLUTION, mIsInResolution);
-	}
-
-	/**
-	 * Handles Google Play Services resolution callbacks.
-	 */
-	/*@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		super.onActivityResult(requestCode, resultCode, data);
-		switch (requestCode) {
-			case REQUEST_CODE_RESOLUTION:
-				retryConnecting();
-				break;
-		}
-	}*//*
-
-	private void retryConnecting() {
-		mIsInResolution = false;
-		if (!mGoogleApiClient.isConnecting()) {
-			mGoogleApiClient.connect();
-		}
-	}
-
-	/**
-	 * Called when {@code mGoogleApiClient} is connected.
-	 *//*
-	@Override
-	public void onConnected(Bundle connectionHint) {
-		Log.i(TAG, "GoogleApiClient connected");
-		// TODO: Start making API requests.
-	}
-
-	/**
-	 * Called when {@code mGoogleApiClient} connection is suspended.
-	 *//*
-	@Override
-	public void onConnectionSuspended(int cause) {
-		Log.i(TAG, "GoogleApiClient connection suspended");
-		retryConnecting();
-	}
-
-	/**
-	 * Called when {@code mGoogleApiClient} is trying to connect but failed.
-	 * Handle {@code result.getResolution()} if there is a resolution
-	 * available.
-	 *//*
-	@Override
-	public void onConnectionFailed(ConnectionResult result) {
-		Log.i(TAG, "GoogleApiClient connection failed: " + result.toString());
-		if (!result.hasResolution()) {
-			// Show a localized error dialog.
-			GooglePlayServicesUtil.getErrorDialog(
-					result.getErrorCode(), this, 0, new OnCancelListener() {
-						@Override
-						public void onCancel(DialogInterface dialog) {
-							retryConnecting();
-						}
-					}).show();
-			return;
-		}
-		// If there is an existing resolution error being displayed or a resolution
-		// activity has started before, do nothing and wait for resolution
-		// progress to be completed.
-		if (mIsInResolution) {
-			return;
-		}
-		mIsInResolution = true;
-		try {
-			result.startResolutionForResult(this, REQUEST_CODE_RESOLUTION);
-		} catch (SendIntentException e) {
-			Log.e(TAG, "Exception while starting resolution activity", e);
-			retryConnecting();
-		}
-	}
-
-	public void onStartMatchClicked(View view) {
-		Intent intent =
-				Games.TurnBasedMultiplayer.getSelectOpponentsIntent(mGoogleApiClient, 1, 7, true);
-		startActivityForResult(intent, RC_SELECT_PLAYERS);
-	}
-
-	@Override
-	public void onActivityResult(int request, int response, Intent data) {
-		super.onActivityResult(request, response, data);
-
-		switch (request) {
-			case REQUEST_CODE_RESOLUTION:
-				retryConnecting();
-				break;
-			case RC_SELECT_PLAYERS:
-				if (response != Activity.RESULT_OK) {
-					// user canceled
-					return;
-				}
-				break;
-
-			// Get the invitee list.
-			final ArrayList<String> invitees =
-					data.getStringArrayListExtra(Games.EXTRA_PLAYER_IDS);
-
-			// Get auto-match criteria.
-			Bundle autoMatchCriteria = null;
-			int minAutoMatchPlayers = data.getIntExtra(
-					Multiplayer.EXTRA_MIN_AUTOMATCH_PLAYERS, 0);
-			int maxAutoMatchPlayers = data.getIntExtra(
-					Multiplayer.EXTRA_MAX_AUTOMATCH_PLAYERS, 0);
-			if (minAutoMatchPlayers > 0) {
-				autoMatchCriteria = RoomConfig.createAutoMatchCriteria(
-						minAutoMatchPlayers, maxAutoMatchPlayers, 0);
-			} else {
-				autoMatchCriteria = null;
-			}
-
-			TurnBasedMatchConfig tbmc = TurnBasedMatchConfig.builder()
-					.addInvitedPlayers(invitees)
-					.setAutoMatchCriteria(autoMatchCriteria)
-					.build();
-
-			// Create and start the match.
-			Games.TurnBasedMultiplayer
-					.createMatch(mGoogleApiClient, tbmc)
-					.setResultCallback(new MatchInitiatedCallback());
-		}
-	}
-
-	public class MatchInitiatedCallback implements ResultCallback<TurnBasedMultiplayer.InitiateMatchResult>
-	{
-
-		@Override
-		public void onResult(TurnBasedMultiplayer.InitiateMatchResult result) {
-			// Check if the status code is not success.
-			Status status = result.getStatus();
-			if (status.isSuccess()) {
-				showError(status.getStatusCode());
-				return;
-			}
-
-			TurnBasedMatch match = result.getMatch();
-
-			// If this player is not the first player in this match, continue.
-			if (match.getData() != null) {
-				showTurnUI(match);
-				return;
-			}
-
-			// Otherwise, this is the first player. Initialize the game state.
-			initGame(match);
-
-			// Let the player take the first turn
-			showTurnUI(match);
-
-		}
-
-		private TurnBasedMatch mMatch;
-		private TextView mDataView;
-
-		// Call this method when a player has completed his turn and wants to
-		// go onto the next player, which may be himself.
-		public void playTurn(View view) {
-
-			// Get the next participant in the game-defined way, possibly round-robin.
-			String nextParticipantId = getNextParticipantId();
-
-			// Get the updated state. In this example, we simply retrieve a
-			// text string from the view. In your game, there may be more
-			// complicated state.
-			mTurnData = mDataView.getText().toString();
-
-			// At this point, you might want to show a waiting dialog so that
-			// the current player does not try to submit turn actions twice.
-			showSpinner();
-
-			// Invoke the next turn. We are converting our data to a byte array.
-			Games.TurnBasedMultiplayer
-					.takeTurn(mGoogleApiClient, mMatch.getMatchId(),
-							mTurnData.getBytes(Charset.forName("UTF-16")),
-							nextParticipantId)
-					.setResultCallback(this);
-		}
-
-	}
-}*/
